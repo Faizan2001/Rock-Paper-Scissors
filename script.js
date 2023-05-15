98b2d6b8665e85a1e597788a0749f508b16e5fec
Faizan Syed <faizan.syed2001@gmail.com>
2023-05-16 00:30:14 +0600

    playRound function has game logic where computerSelection and playerSelection are it's parameters

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 function getComputerChoice(list) {
-  
-return list[Math.floor(Math.random()*list.length)];
+
+    return list[Math.floor((Math.random() * list.length))];
 
 }
 
 
-console.log(getComputerChoice(["Rock", "Paper", "Scissors"]))
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+
+
+    if (playerSelection == computerSelection) {
+        console.log("Tie!")
+    } else 
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection)
+        } else if (computerSelection == "paper") {
+            console.log("You Win! " + playerSelection + " beats " + computerSelection)
+        }
+    } else
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            console.log("You Win! " + playerSelection + " beats " + computerSelection)
+        } else if (computerSelection == "scissors") {
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection)
+        }
+    } else 
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection)
+        } else if (computerSelection == "scissors") {
+            console.log("You Win! " + computerSelection + " beats " + playerSelection)
+        }
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice(["Rock", "Paper", "Scissors"])
+console.log("Player: " + playerSelection)
+console.log("Computer: " + computerSelection)
+
+console.log(playRound(playerSelection, computerSelection));
